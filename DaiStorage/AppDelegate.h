51ece905b7783e9d7dc873e59b46fd51a8a14ff7
DaidoujiChen <daidoujichen@gmail.com>
2015-04-08 17:36:00 +0800

    Initial Commit

---
+++ DaiStorage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DaiStorage
+//
+//  Created by DaidoujiChen on 2015/4/8.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DaiStorage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaiStorage
+//
+//  Created by DaidoujiChen on 2015/4/8.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
