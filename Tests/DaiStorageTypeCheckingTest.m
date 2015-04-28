f5b0e6c394cc78fe8ecf88e5a3d8c3cdef3b80eb
DaidoujiChen <daidoujichen@gmail.com>
2015-04-28 13:05:11 +0800

    修正 DaiStorageTypeCheckingTest 測項

--- Tests/DaiStorageTypeCheckingTest.m
+++ Tests/DaiStorageTypeCheckingTest.m
@@ -4,7 +4,7 @@
 #import <GHUnit/GHUnit.h>
 
 #import "DaiStorageTypeChecking.h"
-#import "DaiStorage.h"
+#import "Classroom.h"
 
 @interface DaiStorageTypeCheckingTest : GHTestCase
 @end
@@ -12,6 +12,12 @@
 @implementation DaiStorageTypeCheckingTest
 
 - (void)testDaiStorageTypeChecking {
+    GHAssertTrue([DaiStorageTypeChecking on:[Classroom shared]] == DaiStorageTypeDaiStorage, @"判斷格式錯誤");
+    GHAssertTrue([DaiStorageTypeChecking on:[Classroom shared].students] == DaiStorageTypeDaiStorageArray, @"判斷格式錯誤");
+    GHAssertTrue([DaiStorageTypeChecking on:[Classroom shared].strings] == DaiStorageTypeDaiStorageArray, @"判斷格式錯誤");
+    
+    NSString *myString = [NSString new];
+    GHAssertTrue([DaiStorageTypeChecking on:myString] == DaiStorageTypeOthers, @"判斷格式錯誤");
 }
 
 @end
